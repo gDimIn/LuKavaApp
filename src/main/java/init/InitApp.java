30b1294602d4593df23952add1099404e74b7ee3
gDimIn <mep3ab4uk1009@gmail.com>
2025-07-27 15:59:51 +0300

    Init first commit

---
+++ src/main/java/init/InitApp.java
@@ -0,0 +1,19 @@
+package init;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class InitApp {
+
+    public static void main(String[] args) {
+
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new UpdateApp());
+            System.out.println("Chat success");
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
